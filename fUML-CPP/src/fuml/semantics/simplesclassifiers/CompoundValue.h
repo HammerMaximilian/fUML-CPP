83cf12b4d95e2ab6c3b3743100deb018cace8d1b
HammerMaximilian <58948559+HammerMaximilian@users.noreply.github.com>
2023-08-23 17:13:58 +0200

    Implemented ::semantics::simpleclassifiers::CompoundValue

--- fUML-CPP/src/fuml/semantics/simplesclassifiers/CompoundValue.h
+++ fUML-CPP/src/fuml/semantics/simplesclassifiers/CompoundValue.h
@@ -8,8 +8,24 @@
 #ifndef FUML_SEMANTICS_SIMPLESCLASSIFIERS_COMPOUNDVALUE_H_
 #define FUML_SEMANTICS_SIMPLESCLASSIFIERS_COMPOUNDVALUE_H_
 
+#include <fuml/semantics/simplesclassifiers/StructuredValue.h>
 
+namespace fuml::semantics::simpleclassifiers
+{
+	class CompoundValue : public StructuredValue
+	{
+		public:
+			FeatureValueListPtr featureValues = std::make_shared<FeatureValueList>();
 
-
+		public:
+			virtual bool equals(const ValuePtr&);
+			virtual ValuePtr copy();
+			virtual FeatureValuePtr getFeatureValue(const StructuralFeaturePtr&);
+			virtual void setFeatureValue(const StructuralFeaturePtr&,
+					const ValueListPtr&, int position);
+			virtual FeatureValueListPtr getFeatureValues();
+			virtual std::string toString();
+	}; // CompoundValue
+}
 
 #endif /* FUML_SEMANTICS_SIMPLESCLASSIFIERS_COMPOUNDVALUE_H_ */
