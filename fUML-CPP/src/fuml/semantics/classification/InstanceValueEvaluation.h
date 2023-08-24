14438e6c69183d127fac86aa4860e94b519838e5
HammerMaximilian <58948559+HammerMaximilian@users.noreply.github.com>
2023-08-24 15:54:21 +0200

    Implemented header for package ::semantics::classification

---
+++ fUML-CPP/src/fuml/semantics/classification/InstanceValueEvaluation.h
@@ -0,0 +1,24 @@
+/*
+ * InstanceValueEvaluation.h
+ *
+ *  Created on: 24.08.2023
+ *      Author: maha6913
+ */
+
+#ifndef FUML_SEMANTICS_CLASSIFICATION_INSTANCEVALUEEVALUATION_H_
+#define FUML_SEMANTICS_CLASSIFICATION_INSTANCEVALUEEVALUATION_H_
+
+#include <fuml/semantics/values/Evaluation.h>
+
+namespace fuml::semantics::classification
+{
+	class InstanceValueEvaluation : Evaluation
+	{
+		public:
+			virtual ~InstanceValueEvaluation() = default;
+
+			virtual ValuePtr evaluate() override;
+	}; //
+}
+
+#endif /* FUML_SEMANTICS_CLASSIFICATION_INSTANCEVALUEEVALUATION_H_ */
