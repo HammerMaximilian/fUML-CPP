782da238b0464362fa50b6634d6ff09dc499f820
HammerMaximilian <58948559+HammerMaximilian@users.noreply.github.com>
2023-08-30 23:20:48 +0200

    Added first rudimentary main application

--- fUML-CPP/src/main.cpp
+++ fUML-CPP/src/main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 using namespace std;
 
+#include <fuml/syntax/activities/Activity.h>
+#include <fuml/semantics/commonbehavior/ParameterValue.h>
+#include <fuml/semantics/loci/Locus.h>
+#include <fuml/semantics/loci/Executor.h>
+#include <fuml/semantics/loci/ExecutionFactory.h>
+
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	ActivityPtr myActivity(new Activity());
+	myActivity->setThisActivityPtr(myActivity);
+	myActivity->name = "MyFirstActivity";
+
+	LocusPtr locus(new Locus());
+	locus->setThisLocusPtr(locus);
+	ExecutorPtr executor(new Executor());
+	ExecutionFactoryPtr executionFactory(new ExecutionFactory());
+
+	locus->setExecutor(executor);
+	locus->setFactory(executionFactory);
+
+	ParameterValueListPtr outputValues(new ParameterValueList());
+
+	outputValues = executor->execute(myActivity, nullptr, ParameterValueListPtr(new ParameterValueList));
+
+	std::cout<<"Done..."<<std::endl;
+
 	return 0;
 }
