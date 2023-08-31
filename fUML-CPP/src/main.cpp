81531876073eb782a7698e0a1af6e3792ceb5a63
HammerMaximilian <58948559+HammerMaximilian@users.noreply.github.com>
2023-08-31 16:52:39 +0200

    Update main.cpp

--- fUML-CPP/src/main.cpp
+++ fUML-CPP/src/main.cpp
@@ -1,11 +1,11 @@
+#include <examples/helloworld/HelloWorldExampleEnvironment.h>
 #include <iostream>
-#include <examples/helloworld/HelloWorldExample.h>
 using namespace std;
 
 int main() {
 
 	std::cout<<__PRETTY_FUNCTION__<<"	Starting..."<<std::endl;
-	examples::HelloWorldExample::HelloWorldExample::Instance()->execute();
+	examples::HelloWorldExample::HelloWorldExampleEnvironment::Instance()->execute();
 	std::cout<<__PRETTY_FUNCTION__<<"	Done..."<<std::endl;
 
 	return 0;
