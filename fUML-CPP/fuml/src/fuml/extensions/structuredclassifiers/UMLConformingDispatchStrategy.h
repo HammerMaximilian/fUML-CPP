82f66cc9970243018b6c026d2394cc31831fe88f
HammerMaximilian <58948559+HammerMaximilian@users.noreply.github.com>
2024-03-07 13:06:59 +0100

    Create UMLConformingDispatchStrategy.h

---
+++ fUML-CPP/fuml/src/fuml/extensions/structuredclassifiers/UMLConformingDispatchStrategy.h
@@ -0,0 +1,20 @@
+/*
+ * UMLConformingDispatchStrategy.h
+ *
+ *  Created on: 07.03.2024
+ *      Author: maha6913
+ */
+
+#ifndef FUML_EXTENSIONS_STRUCTUREDCLASSIFIERS_UMLCONFORMINGDISPATCHSTRATEGY_H_
+#define FUML_EXTENSIONS_STRUCTUREDCLASSIFIERS_UMLCONFORMINGDISPATCHSTRATEGY_H_
+
+#include <fuml/extensions/structuredclassifiers/SignatureBasedDispatchStrategy.h>
+
+namespace fuml::extensions::structuredclassifiers
+{
+	class UMLConformingDispatchStrategy : public SignatureBasedDispatchStrategy
+	{
+	}; // UMLConformingDispatchStrategy
+}
+
+#endif /* FUML_EXTENSIONS_STRUCTUREDCLASSIFIERS_UMLCONFORMINGDISPATCHSTRATEGY_H_ */
